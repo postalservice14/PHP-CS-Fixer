6951c07d2364dd3992041de857c998d76f7b376a
John Kelly <john.m.kelly@adp.com>
2013-01-16 10:29:17 -0800

    Add a fixer for when there is a space after a function call

---
+++ Symfony/CS/Fixer/SpaceAfterClosingFunctionCallFixer.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Fixer;
+
+use Symfony\CS\FixerInterface;
+
+/**
+ * @author John Kelly <johnmkelly86@gmail.com>
+ */
+class SpaceAfterClosingFunctionCallFixer implements FixerInterface
+{
+    /**
+     * Fixes a file.
+     *
+     * @param \SplFileInfo $file    A \SplFileInfo instance
+     * @param string       $content The file content
+     *
+     * @return string The fixed file content
+     */
+    public function fix(\SplFileInfo $file, $content)
+    {
+        return preg_replace('/\)\s+;/', ');', $content);
+    }
+
+    /**
+     * Returns the level of CS standard.
+     *
+     * Can be one of self::PSR1_LEVEL, self::PSR2_LEVEL, or self::ALL_LEVEL
+     */
+    public function getLevel()
+    {
+        // defined in PSR-2 4.3
+        return FixerInterface::PSR2_LEVEL;
+    }
+
+    /**
+     * Returns the priority of the fixer.
+     *
+     * The default priority is 0 and higher priorities are executed first.
+     */
+    public function getPriority()
+    {
+        return 0;
+    }
+
+    /**
+     * Returns true if the file is supported by this fixer.
+     *
+     * @param \SplFileInfo $file
+     * @return bool true if the file is supported by this fixer, false otherwise
+     */
+    public function supports(\SplFileInfo $file)
+    {
+        return 'php' == pathinfo($file->getFilename(), PATHINFO_EXTENSION);
+    }
+
+    /**
+     * Returns the name of the fixer.
+     *
+     * The name must be all lowercase and without any spaces.
+     *
+     * @return string The name of the fixer
+     */
+    public function getName()
+    {
+        return 'space_after_closing_function_call';
+    }
+
+    /**
+     * Returns the description of the fixer.
+     *
+     * A short one-line description of what the fixer does.
+     *
+     * @return string The description of the fixer
+     */
+    public function getDescription()
+    {
+        return 'Space after closing parentheses of method call is prohibited.';
+    }
+}
---
+++ Symfony/CS/Tests/Fixer/SpaceAfterClosingFunctionCallFixerTest.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * This file is part of the Symfony CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Fixer;
+
+use Symfony\CS\Fixer\SpaceAfterClosingFunctionCallFixer;
+
+class SpaceAfterClosingFunctionCallFixerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideClosingTagExamples
+     */
+    public function testOneLineFix($expected, $input)
+    {
+        $fixer = new SpaceAfterClosingFunctionCallFixer();
+        $file = new \SplFileInfo(__FILE__);
+
+        $this->assertEquals($expected, $fixer->fix($file, $input));
+    }
+
+    public function provideClosingTagExamples()
+    {
+        return array(
+            array('<?php $this->foo(); ?>', '<?php $this->foo() ; ?>'),
+            array(
+                '<?php $this->foo(\'with param containing ;\'); ?>',
+                '<?php $this->foo(\'with param containing ;\') ; ?>'
+            ),
+            array('<?php $this->foo(); ?>', '<?php $this->foo(); ?>'),
+            array('<?php
+
+$this->foo();
+
+?>
+',
+                '<?php
+
+$this->foo() ;
+
+?>
+')
+        );
+    }
+}
